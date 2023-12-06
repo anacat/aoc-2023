5337ef28ec59d63d12655d884f85683683302a10
anacat <catarina.lopes.04@hotmail.com>
2023-12-06 16:40:59 +0000

    day 4 - part 2

--- AdventOfCode/Day4.cpp
+++ AdventOfCode/Day4.cpp
@@ -21,7 +21,7 @@ int Day4::main()
 			//it is what it is, im not sorry
 
 			ss >> card >> game >> char1 >> winningNrs[0] >> winningNrs[1] >> winningNrs[2] >> winningNrs[3] >>
-				winningNrs[4] >> winningNrs[5]  >> winningNrs[6] >> winningNrs[7] >> winningNrs[8] >> winningNrs[9] >>
+				winningNrs[4] >> winningNrs[5] >> winningNrs[6] >> winningNrs[7] >> winningNrs[8] >> winningNrs[9] >>
 				char2 >> playedNrs[0] >> playedNrs[1] >> playedNrs[2] >> playedNrs[3] >> playedNrs[4] >>
 				playedNrs[5] >> playedNrs[6] >> playedNrs[7] >> playedNrs[8] >> playedNrs[9] >> playedNrs[10] >>
 				playedNrs[11] >> playedNrs[12] >> playedNrs[13] >> playedNrs[14] >> playedNrs[15] >> playedNrs[16] >>
@@ -46,9 +46,8 @@ int Day4::main()
 		cout << "Unable to open file." << endl;
 	}
 
-	cardList.shrink_to_fit();
-
 	int sum = 0;
+	int size = cardList.size();
 
 	for (int i = 0; i < cardList.size(); i++) {
 		int score = 0;
@@ -64,16 +63,37 @@ int Day4::main()
 					else {
 						score *= 2;
 					}
+
+					cardList[i].score += 1;
 				}
 			}
 		}
 
 		sum += score;
+	}
 
-		cout << score << endl;
+	int nrCards = cardList.size();
+	vector<ScratchCard> copy = cardList;
+
+	for (int i = 0; i < cardList.size(); i++) {
+		int cardNr = cardList[i].cardNr;
+
+		for (int j = 1; j <= cardList[i].score; j++) {
+			if (cardList[i].score < nrCards)
+			{
+				ScratchCard newCard = copy[cardNr - 1 + j];
+
+				if (newCard.cardNr != cardNr) {
+					copy[cardNr - 1 + j].nCopies++;
+					newCard.isCopy = true;
+					cardList.push_back(newCard);
+				}
+			}
+		}
 	}
 
 	cout << sum << endl;
+	cout << cardList.size() << endl;
 
 	return 0;
 }
--- AdventOfCode/Day4.h
+++ AdventOfCode/Day4.h
@@ -15,8 +15,13 @@ public:
 private:
 	struct ScratchCard {
 		int cardNr;
+		int nCopies = 1;
+		int score = 0;
 		vector<int> winningNrs;
 		vector<int> playedNrs;
+
+		bool isCopy = false;
+		vector<ScratchCard> winnings;
 	};
 };
 
